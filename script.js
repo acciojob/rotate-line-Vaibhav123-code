75a02ad17b0a725db3e9ca6fd555ed34ec73b651
Vaibhav123-code <124595249+Vaibhav123-code@users.noreply.github.com>
2023-05-05 23:19:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+line.style.animation = 'rotate 3s linear infinite';
+
+// Add the necessary CSS styles to the page header to define the rotate animation
+const style = document.createElement('style');
+style.innerHTML = `
+  @keyframes rotate {
+    from { transform: rotate(0deg); }
+    to { transform: rotate(360deg); }
+  }
+`;
+document.head.appendChild(style);
